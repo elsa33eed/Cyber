2ae21de31579aa21c05f8dec04402fccfc3c3839
Abdallah Atef <abdallaatef12@google.com>
2024-04-27 22:34:10 +0300

    updating bcrypt library

--- Server/Controllers/Users.js
+++ Server/Controllers/Users.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const UserModel = require('../Models/Users');
 const LoginValid = require('../Utils/LoginValid');
